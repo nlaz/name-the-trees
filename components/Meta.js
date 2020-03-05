9f509f6fb9e0f763c1eaef8fe4ba6f40c2aa597d
Niko <niko@mlh.io>
2020-03-04 20:43:07 -0500

    Setup project structure

---
+++ components/Meta.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import Head from "next/head";
+
+const rootUrl = "http://niko.pizza"; // Replace with url
+
+const defaults = {
+  company: "Name the Trees",
+  url: rootUrl,
+  twitterImage: "/images/social-cards/twitter-card.jpg",
+  facebookImage: "/images/social-cards/facebook-card.jpg"
+};
+
+class Meta extends Component {
+  componentDidMount() {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+  }
+
+  render() {
+    let canonicalUrl = `${rootUrl}${this.props.path}`;
+    let twitterImage = `${rootUrl}${this.props.twitterImage ||
+      defaults.twitterImage}`;
+    let facebookImage = `${rootUrl}${this.props.facebookImage ||
+      defaults.facebookImage}`;
+
+    return (
+      <Head>
+        <title>{this.props.title}</title>
+        <meta name="description" content={this.props.description} />
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+
+        <meta property="og:title" content={this.props.title} />
+        <meta property="og:description" content={this.props.description} />
+        <meta property="og:site_name" content="Major League Hacking" />
+        <meta property="fb:app_id" content="1612424139009027" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content={facebookImage} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:url" content={canonicalUrl} />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={this.props.title} />
+        <meta name="twitter:description" content={this.props.description} />
+        <meta name="twitter:image" content={twitterImage} />
+        <link rel="stylesheet" href="https://unpkg.com/tachyons@4/css/tachyons.min.css">
+        <link rel="stylesheet" href="/styles/index.css" />
+      </Head>
+    );
+  }
+}
+
+export default Meta;
---
+++ sections/HeaderSection.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HeaderSection = () => (
+  <div>
+    <h1>Header</h1>
+  </div>
+);
+
+export default HeaderSection;
