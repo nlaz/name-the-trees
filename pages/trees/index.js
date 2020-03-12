f26313f1377a582fd1c22458f867ce944229058e
Niko <niko@mlh.io>
2020-03-12 08:18:09 -0400

    Collapsable sidebar

--- pages/trees/index.js
+++ pages/trees/index.js
@@ -1,33 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
 
 import Meta from "../../components/Meta";
 import Navbar from "../../components/Navbar";
 import Sidebar from "../../sections/Sidebar";
 import MapView from "../../sections/MapView";
 
-const TreesPage = () => {
-  const [isOpen, setOpen] = useState(true);
-
-  return (
-    <div
-      className="container helvetica bg-washed-green flex flex-column"
-      style={{ minHeight: "100vh" }}
-    >
-      <Meta
-        title="Name Those Trees"
-        description="Name Those Trees is a tree naming service that raises funds for local services."
-        path="/trees"
-      />
-      <Navbar />
-      <div className="flex relative" style={{ flex: "1" }}>
-        <MapView className="relative" />
-        <Sidebar
-          className="w-25 br absolute top-0 bottom-0 left-0 right-0"
-          onClose={() => setOpen(false)}
-        />
-      </div>
+const TreesPage = () => (
+  <div
+    className="container helvetica bg-washed-green flex flex-column"
+    style={{ minHeight: "100vh" }}
+  >
+    <Meta
+      title="Name Those Trees"
+      description="Name Those Trees is a tree naming service that raises funds for local services."
+      path="/trees"
+    />
+    <Navbar />
+    <div className="flex relative" style={{ flex: "1" }}>
+      <MapView className="relative" />
+      <Sidebar className="absolute" />
     </div>
-  );
-};
+  </div>
+);
 
 export default TreesPage;
--- sections/Sidebar.js
+++ sections/Sidebar.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { ChevronsLeft } from "react-feather";
+import React, { useState } from "react";
+import { ChevronsLeft, ChevronsRight } from "react-feather";
 
 const Step = ({ number, children }) => (
   <div className="flex items-start mv3">
@@ -15,32 +15,51 @@ const Step = ({ number, children }) => (
   </div>
 );
 
-const Sidebar = ({ className, onClose }) => (
-  <div className={`bg-washed-green ${className} ph3 pv3 relative`}>
-    <h1 className="f4 mb2 relative">Tree Naming Services</h1>
-    <p className="f6 mt0 mb4 dark-gray">
-      Buy the online naming rights for a tree in New York City and support local
-      services.
-    </p>
-    <div className="bb b--dark-gray w-100" />
-    <div className="pv3">
-      <Step number="1">
-        Find a tree in your local area. One that you really bond with.
-      </Step>
-      <Step number="2">
-        Think of a good name for your tree. Tree puns are particularly helpful.
-      </Step>
-      <Step number="3">
-        Make it official. Name your tree through our tree naming portal.
-      </Step>
-    </div>
+const Sidebar = ({ className, onClose }) => {
+  const [isOpen, setOpen] = useState(true);
+
+  if (!isOpen) {
+    return (
+      <div
+        className="flex items-center justify-center bg-washed-green ma3 absolute top-0 left-0 br2 ba"
+        style={{ width: "40px", height: "40px" }}
+        onClick={() => setOpen(true)}
+      >
+        <ChevronsRight />
+      </div>
+    );
+  }
+
+  return (
     <div
-      className="absolute top-0 right-0 mv2 mh2 pa2 pointer"
-      onClick={onClose}
+      className={`${className} w-25 bg-washed-green ph3 pv3 relative top-0 bottom-0 left-0 right-0 br`}
     >
-      <ChevronsLeft />
+      <h1 className="f4 mb2 relative">Tree Naming Services</h1>
+      <p className="f6 mt0 mb4 dark-gray">
+        Buy the online naming rights for a tree in New York City and support
+        local services.
+      </p>
+      <div className="bb b--dark-gray w-100" />
+      <div className="pv3">
+        <Step number="1">
+          Find a tree in your local area. One that you really bond with.
+        </Step>
+        <Step number="2">
+          Think of a good name for your tree. Tree puns are particularly
+          helpful.
+        </Step>
+        <Step number="3">
+          Make it official. Name your tree through our tree naming portal.
+        </Step>
+      </div>
+      <div
+        className="absolute top-0 right-0 mv2 mh2 pa2 pointer"
+        onClick={() => setOpen(false)}
+      >
+        <ChevronsLeft />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Sidebar;
